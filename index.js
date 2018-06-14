f8ffc8d23e0848770649a1867f8c9575592fc0a2
Fang1459761245 <1459761245@qq.com>
2018-06-14 10:23:59 +0800

    Fang 修改了index.js文件

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var oLi = document.getElementsByTagName("li");
 for(var i=0;i<oLi.length;i++){
     oLi[i].index = i;
     oLi[i].onclick=function(){
-        
+        for(var j=0;j<oLi.length;j++){
+        	oLi[j].className="";
+        }
+        oLi[this.index].className="active";
     }
 }
